2a56187a13440a95c39c60946ad0229ad0b0abd4
Daniel Liu <danliu277@gmail.com>
2019-12-10 18:45:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+class School
+  attr_accessor :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+      @roster[grade] = []
+      
+      @roster[grade].push(name)
+end
